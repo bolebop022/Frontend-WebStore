df0df97ff6257cc1d92b7e91a626e99d3cd447a8
Bongane Lebopo <bonganelebop@gmail.com>
2025-04-24 17:07:48 +0200

    Modified.

--- frontend-webstore/src/app/services/product.service.ts
+++ frontend-webstore/src/app/services/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/products';
+
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(): Observable<Product[]>
 }
